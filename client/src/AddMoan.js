e7bf05e62cbc4cf8251d8ed1742d131d51bc508f
Arildsen <andr0910@hotmail.com>
2021-11-29 13:12:12 +0100

    lidt styling men ikke for meget

--- client/src/AddMoan.js
+++ client/src/AddMoan.js
@@ -1,5 +1,5 @@
 import {useState} from "react";
-
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 function AddMoan(props){
     const{addMoan} = props;
     const[headline, setHeadline] = useState("");
@@ -9,25 +9,28 @@ function AddMoan(props){
         <div>
             <h3>Moan</h3>
             <div>
-                <label htmlFor="headline">Who or what is responsible for your moaning</label>
+                <label htmlFor="headline">Who or what is responsible for your moaning</label><br/>
                 <input
                 name="headline"
                 id="headline"
                 onChange={(event) => setHeadline(event.target.value)}
                 type="text"
+                
                 />
             </div>
             <div>
-                <label htmlFor="complaint">Elaborate for more sympathy and venting</label>
+                <label htmlFor="complaint">Elaborate for more sympathy and venting</label><br/>
                 <input
                 name="complaint"
                 id="complaint"
                 onChange={(event) => SetComplaint(event.target.value)}
                 type="text"
+                
                 />
             </div>
             <button 
             type="button"
+            className="btn-danger"
             onClick={(event) =>{
             addMoan(headline,complaint);
                 
